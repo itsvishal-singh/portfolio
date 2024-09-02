5f294ea49e4cf974055933ac3dcbd8e37bbbedb0
Vishal <vishal.200087@gmail.com>
2024-09-02 10:22:32 +0530

    Mail and Social link

--- src/Components/Mail.tsx
+++ src/Components/Mail.tsx
@@ -1,6 +1,6 @@
 const Mail=()=>{
   return <div className="flex md-mx:hidden text-textColor items-center gap-10 fixed bottom-32 -right-48 rotate-90">
-      <div className="flex" data-aos-duration="800" data-aos="fade-down-left">
+      <div className="flex aos-init aos animate" data-aos-duration="800" >
       <a  href="mailto:vishal.200085@gmail.com" className="font-mono tracking-wide hover:text-primaryColor hover:-translate-x-1 transition transform duration-300 ease-in-out">
           vishal.200085@gmail.com
       </a>
