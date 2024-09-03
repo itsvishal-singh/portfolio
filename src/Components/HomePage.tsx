adcc206e43d4247f740f7ad6d229df64ea7ed979
Vishal <vishal.200087@gmail.com>
2024-09-03 19:47:47 +0530

    Portfolio

--- src/Components/HomePage.tsx
+++ src/Components/HomePage.tsx
@@ -1,7 +1,11 @@
 import About from "./About";
+import Contact from "./Contact";
+import Experience from "./Experience";
+import Footer from "./Footer";
 import Header from "./Header";
 import Mail from "./Mail";
 import Projects from "./Projects";
+import Skills from "./Skills";
 import Social from "./Social";
 
 const HomePage=()=>{
@@ -10,6 +14,11 @@ const HomePage=()=>{
         <Header/>
         <About/>
         <Projects/>
+        <Skills/>
+        <Experience/>
+
+        <Contact/>
+        <Footer/>
         <Mail/>
         <Social/>
         </>
