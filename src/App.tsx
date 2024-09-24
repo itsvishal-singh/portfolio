1cfe5ff44b289631366b67d12f1e9c0a66b21b4e
Vishal <vishal.200087@gmail.com>
2024-09-24 09:12:02 +0530

    Portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-// import React, { useEffect } from 'react';
-// import AOS from 'aos';
-import 'aos/dist/aos.css';
-import {  MantineProvider } from '@mantine/core';
-import '@mantine/core/styles.css';
+import "@mantine/core/styles.css";
 import HomePage from './Components/HomePage';
+import { MantineProvider, createTheme } from '@mantine/core';
+import { pdfjs } from 'react-pdf';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 function App() {
+  useEffect(()=>{
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
+  const theme = createTheme({
+    breakpoints: {
+      'xs': '320px',
+      'sm': '476px',
+      'md': '640px',
+      'bs': '768px',
+      'lg': '900px',
+      'xl': '1024',
+      '2xl': '1280',
+    },
+  });
   return (
-    <MantineProvider>
-      <HomePage/>
+    <MantineProvider theme={theme} >
+    <HomePage/>
     </MantineProvider>
   );
 }
 
 export default App;
- 
